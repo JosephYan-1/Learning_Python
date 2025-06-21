8f6036f81900f708b060b97afd748934d708f8b4
JosephY <josleemaker6@gmail.com>
2025-06-21 10:26:31 -0400

    implemented list of 50 words in hangman_list started visual showing of hangman in hangman.py

--- hangman.py
+++ hangman.py
@@ -4,3 +4,38 @@
 #   \O/
 #    |
 #   / \ == 6 tries, once i get strings started i will implement a hangman function
+
+hangman_visual = [
+    "---",      # 0 
+    "  |",      # 1
+    "  |",      # 2
+    "   ",     # 3
+    "   ",      # 4
+    "   ",     # 5
+]
+
+def mistake(count):
+    match count:
+        case 1:
+            hangman_visual[3] = "  O"
+        case 2:
+            hangman_visual[3] = " \\O"
+        case 3: 
+            hangman_visual[3] += "/"
+        case 4: 
+            hangman_visual[4] = "  |"
+        case 5:
+            hangman_visual[5] = " /"
+        case 6: 
+            hangman_visual[5] += " \\"
+    
+    print(f"NOT FOUND, Mistake: {count}\n Tries left: {6-count}")
+
+def print_hangman():
+    for element in hangman_visual:
+        print(element)
+
+# print_hangman()
+# for i in range(7):
+#     mistake(i) #this prints full hangman
+# print_hangman()
---
+++ hangman_list.py
@@ -0,0 +1,5 @@
+words = ["lantern", "crisp", "vortex", "pencil", "marble", "hazard", "fringe", "goblin", "echo", "syrup",
+"thunder", "cobweb", "jigsaw", "plasma", "velvet", "anchor", "raven", "turquoise", "quartz", "spindle",
+"breeze", "flicker", "neutron", "glimmer", "saddle", "ember", "canvas", "pylon", "orbit", "nectar",
+"gravel", "shard", "fungus", "badge", "ripple", "drizzle", "walnut", "zodiac", "mirror", "cloak",
+"nimbus", "quiver", "bunker", "ladle", "static", "rubble", "mantis", "lichen", "tundra", "spigot"]
