756878de6fbd2ef9135f6dc05318db0b04e4a45b
JosephY <josleemaker6@gmail.com>
2025-06-21 09:56:49 -0400

    Finished strings.py uses different methods of accessing list and list elements

--- strings.py
+++ strings.py
@@ -4,10 +4,16 @@
 #now we will make sure the filepath passed in contains:
 #   dir
 #   ext
+# import strings_var #just to use other variables from different files.
+from strings_var import ran_paths, TD_ran_paths
 import os
+
 
 def check_for_dir(path):
     #check for both / and \
+    if (path.startswith("./") or path.startswith(".\\")):
+        return "Current DIR"
+    
     index = path.rfind("/")
     if (index < 0):
         index = path.rfind("\\")
@@ -15,7 +21,7 @@ def check_for_dir(path):
     if (index < 0): #if it found either slash it should not be -1
         return "NO DIR"
     
-    return path[:index]
+    return path[:index+1]
     
 def get_ext(path):
     index = path.rfind(".")
@@ -25,6 +31,8 @@ def get_ext(path):
 
 #filenames can be in current dir and some files may not have an extension
 def get_filename(path):
+    if (path.startswith("./") or path.startswith(".\\")):
+        return path[2:]
     index_ext = path.rfind(".")
     if (index_ext < 0):
         index_ext = len(path)
@@ -40,24 +48,57 @@ def get_filename(path):
         index_dir += 1 
     return path[index_dir:index_ext]
 
-def write_to_file(filename, exists):
-    return
+# def write_to_file(filename):
+#     with open(filename, "w") as file: # w overwrites the file and a appends to the end of the file
+    
+    #return
+def check_exists(path):
+   if (os.path.exists(path)):
+    print(f"FILE EXISTS: {path} ")
+   else:
+    print(f"FILE DOES NOT EXIST!, Creating {path}")
+    file = open(path, "x") # just creating the file 
+    file.close()
 
 filepath = input("give me filepath: ")
 
 #do stuff with the filepath
 
-print(f"Directory: ", check_for_dir(filepath))
+print(f"Directory: ",check_for_dir(filepath))
 print(f"Extension: ",get_ext(filepath))
 print("File name: ", get_filename(filepath))
-
+5
 # going to write to a file 
 # os is being used to check if the file exists or not.
 
 writable_file = "./test.txt" #itll be within the working the dir
-if (os.path.exists(writable_file)):
-    print("FILE EXISTS, WRITING TO FILE")
-else:
-    print(f"FILE DOES NOT EXIST!, Creating {writable_file}")
+check_exists(writable_file)
+# ran_paths = strings_var.ran_paths # if we do not use use __ from file.py we have to do var.___
+# TD_ran_path = strings_var.TD_ran_paths
+with open(writable_file, "w") as file: # do not want to continuely increase file size
+    file.write("USING 1D List\n")
+    for i in range(len(ran_paths)):
+        file.write(f"Path: {ran_paths[i]}\n")
+        file.write(f"\tDir: {check_for_dir(ran_paths[i])}\n")
+        file.write(f"\tExt: {get_ext(ran_paths[i])}\n")
+        file.write(f"\tFile: {get_filename(ran_paths[i])}\n")
+    file.write("END\n")
+    print("Wrote to file")
+
+with open(writable_file, "a") as file: #using append, it should write to the end 
+    file.write("USING 2D List\n")
+    for sublist in TD_ran_paths: #diff waw to access stuff
+        for item in sublist:
+            file.write(f"Path: {item}\n")
+            file.write(f"\tDir: {check_for_dir(item)}\n")
+            file.write(f"\tExt: {get_ext(item)}\n")
+            file.write(f"\tFile: {get_filename(item)}\n")
+        file.write("END\n")
+    print("Wrote to file")
+
+
+
+
+
 
 
---
+++ strings_var.py
@@ -0,0 +1,5 @@
+#MADE ONLY TO BE USED IN strings.py
+#This shows how to use .py file with another .py
+
+ran_paths = ["C:/WOW", "D:/seriously/file.txt", "./field.pdf" ]
+TD_ran_paths = [["ran/path/file.txt", "C:/Us3r/gen.pdf"], ["./current.txt", "field"]]
